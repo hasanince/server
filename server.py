10b525d7bc4ad4d4fe7eeb96a9add56730ed44c5
hasanince <alhasanince@gmail.com>
2020-10-29 15:43:25 +0300

    Update server.py

--- server.py
+++ server.py
@@ -293,7 +293,7 @@ def _setupVNC():
   my_apt.installDebPackage("virtualgl.deb")
   my_apt.installDebPackage("turbovnc.deb")
 
-  my_apt.installPkg("xfce4", "xfce4-terminal",'firefox','vlc')
+  my_apt.installPkg("xfce4", "xfce4-terminal")
   my_apt.commit()
   my_apt.close()
 
